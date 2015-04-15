39f1f99d20f2e296fe41497ef9b7c330c2c53631
kloftas <bengt62.nordberg@gmail.com>
2015-04-15 05:45:53 +0000

    strul

---
+++ javascript/charts.js
@@ -0,0 +1,68 @@
+  $(function() {
+  	$('#container').highcharts({
+  		chart: {
+  			zoomType: 'x',
+  			height: 500,
+  			width: 500
+  		},
+  		title: {
+  			text: 'Temperatur'
+  		},
+  		xAxis: {
+  			categories: <? php
+  			$time[] = [];
+  			foreach($datetimes as $val) {
+  				$time[] = date("H:i:s", strtotime($val));
+  			}
+  			echo json_encode($time); ?>
+  		},
+  		yAxis: {
+  			max: 20,
+  			min: -10,
+  			title: {
+  				text: 'Temperatur'
+  			}
+  		},
+  		series: [{
+  			data: [ <? php echo join($data, ',') ?> ],
+  			type: 'area',
+  			pointStart: 0,
+  		}]
+  	});
+  })
+  $(function() {
+  	$('#container_avg_day').highcharts({
+  		chart: {
+  			zoomType: 'x'
+  		},
+  		title: {
+  			text: 'Daglig medeltemperatur'
+  		},
+  		xAxis: {
+  			categories: <? php
+  			$dayAvg[] = [];
+  			foreach($days as $i) {
+  				$dayAvg[] = date("d/m-y", strtotime($i));
+  			}
+  			echo json_encode($dayAvg); ?>
+  		},
+  		yAxis: {
+  			title: {
+  				text: 'Temperatur'
+  			}
+  		},
+  		series: [{
+  			data: [ <? php echo join($avgDay, ',') ?> ],
+  			type: 'area',
+  			pointStart: 0,
+  		}]
+  	});
+  })
+
+  $(document).ready(function() {
+  	$('#summerYet').text("<?php echo $summerYet ?>");
+  });
+  $('.bg-holder').parallaxScroll({
+  	friction: 0.5
+  });
+  
